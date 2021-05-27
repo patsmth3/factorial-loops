ea4d29e138b2b18b3bcf51b6f130af527366a00c
Pat Smith <patsmith@gmail.com>
2021-05-27 09:52:56 -0400

    factorial using loops

---
+++ Factorial/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the nth value as an integer: ");
+            
+            var n = Convert.ToInt32(Console.ReadLine());
+            
+            //loo calculatioh
+
+            Console.WriteLine($"LOOP calculation: Factorial of {n} is {CalculateLoop(n)}");
+            Console.WriteLine($"LOOP2 calculation: Factorial of {n} is {CalculateLoop(n)}");
+        }
+
+        private static int CalculateLoop(int n)
+        {
+            var factorial = 1;
+            
+            for (int i = n; i >= 1; i--)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+
+        private static int CalculateLoop2(int n)
+        {
+            var factorial = n;
+
+            for (int i = 1; i < n; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+    }
+}
